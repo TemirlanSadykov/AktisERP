ceed416dd19d831f73c6a1d00bb635e84fd1ff3a
TemirlanSadykov <temirlansadykov2004@gmail.com>
2024-04-06 15:49:58 +0900

    Static files

--- aktis/settings.py
+++ aktis/settings.py
@@ -28,12 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -127,6 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
