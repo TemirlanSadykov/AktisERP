41f2a5a7436f3d675edb3bebec549fee0b25b838
TemirlanSadykov <temirlansadykov2004@gmail.com>
2024-04-10 20:00:36 +0900

    Mixin update

--- production/mixins.py
+++ production/mixins.py
@@ -5,18 +5,14 @@ User = get_user_model()
 class AssignBranchMixin:
     def form_valid(self, form):
         response = super().form_valid(form)
-        if hasattr(self.object, 'profile'):
+        # Ensure the User object has a related UserProfile
+        if hasattr(self.object, 'userprofile'):
+            # Set the UserProfile's branch to the current user's branch
             self.object.userprofile.branch = self.request.user.userprofile.branch
             self.object.userprofile.save()
         return response
     
 class RestrictBranchMixin:
-    """
-    A mixin to restrict querysets in class-based views to the current user's branch.
-    """
     def get_queryset(self):
-        """
-        Override the default queryset to filter by the user's associated branch.
-        """
         queryset = super().get_queryset()
         return queryset.filter(branch=self.request.user.userprofile.branch)
