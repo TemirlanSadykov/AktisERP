1501dcc7d1c754fe68f4da7397d902eb4b9bb3ed
TemirlanSadykov <temirlansadykov2004@gmail.com>
2024-04-10 21:30:16 +0900

    Branches for technologist

--- production/urls.py
+++ production/urls.py
@@ -62,6 +62,8 @@ urlpatterns = [
     path('technologist/passports/reassign-work/', reassign_work, name='reassign_work'),
     path('technologist/passports/api/reassigned_works/<int:assigned_work_id>/', get_reassigned_works, name='get_reassigned_works'),
     path('technologist/passports/reassign-work/complete/', complete_reassigned_work, name='complete_reassigned_work'),
+    path('technologist/orders/', OrderListTechnologistView.as_view(), name='order_list_technologist'),
+    path('technologist/orders/<int:pk>/', OrderDetailTechnologistView.as_view(), name='order_detail_technologist'),
 
     path('technologist/operations/', OperationListView.as_view(), name='operation_list'),
     path('technologist/operations/create/', OperationCreateView.as_view(), name='operation_create'),
