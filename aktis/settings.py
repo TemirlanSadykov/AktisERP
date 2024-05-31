3a6ee3594860128464eab3be740e0c17a674055b
TemirlanSadykov <temirlansadykov2004@gmail.com>
2024-06-01 00:22:20 +0900

    New DB Password

--- aktis/settings.py
+++ aktis/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': 'zirmhuJRrDdQtKdboryareiLLjXGUncU',
+        'PASSWORD': 'KygiiflpkqDTcZoiovDCfHsdbIcKsEPD',
         'HOST': 'monorail.proxy.rlwy.net',
         'PORT': '32670',
     }
