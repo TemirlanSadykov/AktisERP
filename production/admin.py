15fc796c919570c2a1746776ba603b70415721d9
TemirlanSadykov <temirlansadykov2004@gmail.com>
2024-08-29 19:25:48 +0600

    Create a branch in superuser panel

--- production/admin.py
+++ production/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import UserProfile
+from .models import Branch
 
 admin.site.register(UserProfile)
+admin.site.register(Branch)
 
 # Register your models here.
