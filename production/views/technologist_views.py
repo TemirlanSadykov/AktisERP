54ede91f6f2faa95423fc1f47b1b738f3d94b64d
TemirlanSadykov <temirlansadykov2004@gmail.com>
2024-09-06 10:36:09 +0900

    Archive button update

--- production/views/technologist_views.py
+++ production/views/technologist_views.py
@@ -1244,7 +1244,7 @@ class EquipmentListView(ListView):
     paginate_by = 10
 
     def get_queryset(self):
-        return Equipment.objects.filter(is_archived=True).order_by('name')
+        return Equipment.objects.filter(is_archived=False).order_by('name')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
