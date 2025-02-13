6e6070ecb02622aab3c6441647d973e7cacff68d
TemirlanSadykov <temirlansadykov2004@gmail.com>
2025-02-13 23:50:09 +0600

    Color & Fabric for Barcode

--- production/views/barcode_views.py
+++ production/views/barcode_views.py
@@ -84,75 +84,75 @@ pdfmetrics.registerFont(TTFont('DejaVuSans', 'static/fonts/DejaVuSans.ttf'))
 
 #         return response
     
-@method_decorator([login_required], name='dispatch')
-class BarcodePassportSize(View):
-    def get(self, request, passport_size_id):
-        passport_size = get_object_or_404(PassportSize, pk=passport_size_id)
-        response = HttpResponse(content_type='application/pdf')
-        response['Content-Disposition'] = f'attachment; filename="passport_{passport_size.passport.id}-size_{passport_size.id}_qrcode.pdf"'
+# @method_decorator([login_required], name='dispatch')
+# class BarcodePassportSize(View):
+#     def get(self, request, passport_size_id):
+#         passport_size = get_object_or_404(PassportSize, pk=passport_size_id)
+#         response = HttpResponse(content_type='application/pdf')
+#         response['Content-Disposition'] = f'attachment; filename="passport_{passport_size.passport.id}-size_{passport_size.id}_qrcode.pdf"'
 
-        # Custom page size with a 30:70 ratio, dimensions in points
-        width, height = 252, 504  # Approximately 3.5 inches by 8.167 inches
+#         # Custom page size with a 30:70 ratio, dimensions in points
+#         width, height = 252, 504  # Approximately 3.5 inches by 8.167 inches
 
-        # Set up PDF with custom size
-        p = canvas.Canvas(response, pagesize=(width, height))
-        p.setFont("DejaVuSans", 14)
+#         # Set up PDF with custom size
+#         p = canvas.Canvas(response, pagesize=(width, height))
+#         p.setFont("DejaVuSans", 14)
 
-        # Top margin and initial text position
-        top_margin = 40 * 2.83464567
-        text_start_height = height - top_margin
+#         # Top margin and initial text position
+#         top_margin = 40 * 2.83464567
+#         text_start_height = height - top_margin
 
-        # Additional Passport Size data
-        date = passport_size.passport.cut.date
-        cut = passport_size.passport.cut.number
-        model = passport_size.passport.cut.order.model.name
-        color = passport_size.passport.roll.color if passport_size.passport.roll else passport_size.passport.cut.order.colors.first()
-        passport = passport_size.passport
-        fabrcis = passport_size.passport.roll.fabrics if passport_size.passport.roll else passport_size.passport.cut.order.fabrics.first()
-        size = passport_size.size_quantity.size
-        quantity = passport_size.quantity
+#         # Additional Passport Size data
+#         date = passport_size.passport.cut.date
+#         cut = passport_size.passport.cut.number
+#         model = passport_size.passport.cut.order.model.name
+#         color = passport_size.passport.roll.color if passport_size.passport.roll else passport_size.passport.cut.order.colors.first()
+#         passport = passport_size.passport
+#         fabrcis = passport_size.passport.roll.fabrics if passport_size.passport.roll else passport_size.passport.cut.order.fabrics.first()
+#         size = passport_size.size_quantity.size
+#         quantity = passport_size.quantity
 
-        p.drawString(10, text_start_height, f"Дата: {date}")
-        p.drawString(10, text_start_height - 25, f"Крой-Паспорт: {passport}")
-        p.drawString(10, text_start_height - 50, f"Модель: {model}")
-        p.drawString(10, text_start_height - 75, f"Цвет: {color}")
-        p.drawString(10, text_start_height - 100, f"Ткань: {fabrcis}")
-        p.drawString(10, text_start_height - 125, f"Размер: {size}")
-        p.drawString(10, text_start_height - 150, f"Количество: {quantity}")
+#         p.drawString(10, text_start_height, f"Дата: {date}")
+#         p.drawString(10, text_start_height - 25, f"Крой-Паспорт: {passport}")
+#         p.drawString(10, text_start_height - 50, f"Модель: {model}")
+#         p.drawString(10, text_start_height - 75, f"Цвет: {color}")
+#         p.drawString(10, text_start_height - 100, f"Ткань: {fabrcis}")
+#         p.drawString(10, text_start_height - 125, f"Размер: {size}")
+#         p.drawString(10, text_start_height - 150, f"Количество: {quantity}")
 
-        # Generate and position QR code
-        qr_data = f"{passport_size.passport.id}-{passport_size.id}"
-        qr = qrcode.QRCode(
-            version=1,
-            box_size=10,
-            border=4,
-        )
-        qr.add_data(qr_data)
-        qr.make(fit=True)
-        img = qr.make_image(fill='black', back_color='white')
+#         # Generate and position QR code
+#         qr_data = f"{passport_size.passport.id}-{passport_size.id}"
+#         qr = qrcode.QRCode(
+#             version=1,
+#             box_size=10,
+#             border=4,
+#         )
+#         qr.add_data(qr_data)
+#         qr.make(fit=True)
+#         img = qr.make_image(fill='black', back_color='white')
 
-        # Save the QR code image to a temporary file
-        temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".png")
-        img.save(temp_file)
-        temp_file.close()
+#         # Save the QR code image to a temporary file
+#         temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".png")
+#         img.save(temp_file)
+#         temp_file.close()
 
-        qr_code_y_position = text_start_height - 375
+#         qr_code_y_position = text_start_height - 375
 
-        # Draw the QR code image on the PDF
-        qr_code_size = 220
-        p.drawImage(temp_file.name, (width - qr_code_size) / 2, qr_code_y_position, width=qr_code_size, height=qr_code_size, mask='auto')
+#         # Draw the QR code image on the PDF
+#         qr_code_size = 220
+#         p.drawImage(temp_file.name, (width - qr_code_size) / 2, qr_code_y_position, width=qr_code_size, height=qr_code_size, mask='auto')
 
-        # Draw the QR data text under the QR code
-        p.setFont("DejaVuSans", 12)
-        p.drawString(10, qr_code_y_position - 10, f"QR Data: {qr_data}")
+#         # Draw the QR data text under the QR code
+#         p.setFont("DejaVuSans", 12)
+#         p.drawString(10, qr_code_y_position - 10, f"QR Data: {qr_data}")
 
-        p.showPage()
-        p.save()
+#         p.showPage()
+#         p.save()
 
-        # Clean up temporary file
-        os.remove(temp_file.name)
+#         # Clean up temporary file
+#         os.remove(temp_file.name)
 
-        return response
+#         return response
     
 @method_decorator([login_required], name='dispatch')
 class BarcodePassportSizePerPiece(View):
@@ -176,11 +176,10 @@ class BarcodePassportSizePerPiece(View):
         text_start_height = height - top_margin
         for piece in pieces:
             date = passport_size.passport.cut.date
-            cut = passport_size.passport.cut.number
             model = passport_size.passport.cut.order.model.name
-            color = passport_size.passport.roll.color if passport_size.passport.roll else passport_size.passport.cut.order.colors.first()
+            color = passport_size.size_quantity.color if passport_size.size_quantity.color else "-"
             passport = passport_size.passport
-            fabrcis = passport_size.passport.roll.fabrics if passport_size.passport.roll else passport_size.passport.cut.order.fabrics.first()
+            fabrcis = passport_size.size_quantity.fabrics if passport_size.size_quantity.fabrics else "-"
             size = f"{passport_size.size_quantity.size} - {passport_size.extra}" if passport_size.extra else passport_size.size_quantity.size
             p.setFont("DejaVuSans", 14)
             p.drawString(10, text_start_height, f"Дата: {date}")
