ad3a2960eb13a803f01de4b64a460a76635e6cc3
TemirlanSadykov <temirlansadykov2004@gmail.com>
2025-05-01 15:30:09 +0900

    Remove print

--- production/views/technologist_views.py
+++ production/views/technologist_views.py
@@ -1127,11 +1127,9 @@ def update_passport_quantity(request):
 
     # Adjust SizeQuantity factual
     size_qty = passport_size.size_quantity
-    print(size_qty.factual)
     if size_qty:
         size_qty.factual = (size_qty.factual or 0) + delta
         size_qty.save()
-    print(size_qty.factual)
     # Update all AssignedWork records
     AssignedWork.objects.filter(work__passport_size=passport_size).update(quantity=new_quantity)
 
